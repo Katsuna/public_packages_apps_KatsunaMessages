0e26756843a7f9bb5a0cfd7f785b3d6814ab13f6
KTakaf <ktakaf@gmail.com>
2016-03-04 16:33:19 +0200

    device

---
+++ app/src/main/java/gr/crystalogic/sms/utils/Device.java
@@ -0,0 +1,74 @@
+package gr.crystalogic.sms.utils;
+
+import android.app.Activity;
+import android.app.AlertDialog;
+import android.content.Context;
+import android.content.DialogInterface;
+import android.content.pm.PackageManager;
+import android.os.Build;
+import android.provider.Telephony;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.content.ContextCompat;
+import android.util.Log;
+
+import gr.crystalogic.sms.BuildConfig;
+import gr.crystalogic.sms.R;
+
+public class Device {
+
+    private static final String TAG = "DEVICE";
+
+    public static boolean isDefaultApp(final Context context) {
+        // there is no default sms app before android 4.4
+        if (Build.VERSION.SDK_INT < Build.VERSION_CODES.KITKAT) {
+            return true;
+        }
+
+        try {
+            // check if this is the default sms app.
+            // If the device doesn't support Telephony.Sms (i.e. tablet) getDefaultSmsPackage() will
+            // be null.
+            final String smsPackage = Telephony.Sms.getDefaultSmsPackage(context);
+            return smsPackage == null || smsPackage.equals(BuildConfig.APPLICATION_ID);
+        } catch (SecurityException e) {
+            // some samsung devices/tablets want permission GET_TASKS o.O
+            Log.e(TAG, "failed to query default SMS app", e);
+            return true;
+        }
+    }
+
+    public static boolean hasPermission(final Context context, final String permission) {
+        return ContextCompat.checkSelfPermission(context, permission)
+                == PackageManager.PERMISSION_GRANTED;
+    }
+
+    public static boolean requestPermission(final Activity activity, final String permission,
+                                            final int requestCode, final int message,
+                                            final DialogInterface.OnClickListener onCancelListener) {
+        Log.i(TAG, "requesting permission: " + permission);
+        if (!hasPermission(activity, permission)) {
+            if (ActivityCompat.shouldShowRequestPermissionRationale(activity, permission)) {
+                new AlertDialog.Builder(activity)
+                        .setTitle(R.string.permissions)
+                        .setMessage(message)
+                        .setCancelable(false)
+                        .setNegativeButton(android.R.string.cancel, onCancelListener)
+                        .setPositiveButton(android.R.string.ok,
+                                new DialogInterface.OnClickListener() {
+                                    @Override
+                                    public void onClick(final DialogInterface dialogInterface,
+                                                        final int i) {
+                                        ActivityCompat.requestPermissions(activity,
+                                                new String[]{permission}, requestCode);
+                                    }
+                                })
+                        .show();
+            } else {
+                ActivityCompat.requestPermissions(activity, new String[]{permission}, requestCode);
+            }
+            return false;
+        } else {
+            return true;
+        }
+    }
+}
