1e3d8ffdd34a4491fecc4bcf056a617e5faf6e28
alkis <a.kafkis@gmail.com>
2016-04-03 18:39:39 +0300

    warnings

--- app/src/main/java/gr/crystalogic/sms/receivers/MmsReceiver.java
+++ app/src/main/java/gr/crystalogic/sms/receivers/MmsReceiver.java
@@ -4,13 +4,9 @@ import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
 
-import gr.crystalogic.sms.utils.Device;
-
 public class MmsReceiver extends BroadcastReceiver {
     @Override
     public void onReceive(Context context, Intent intent) {
-        if (Device.isDefaultApp(context)) {
-
-        }
+        // not implemented
     }
 }
