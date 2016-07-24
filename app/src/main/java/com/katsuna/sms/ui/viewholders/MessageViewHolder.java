857516b93df3ce345bff78920e774d13b1647619
alkis <a.kafkis@gmail.com>
2016-07-24 21:00:15 +0300

    layout adjustments

--- app/src/main/java/com/katsuna/sms/ui/viewholders/MessageViewHolder.java
+++ app/src/main/java/com/katsuna/sms/ui/viewholders/MessageViewHolder.java
@@ -1,6 +1,5 @@
 package com.katsuna.sms.ui.viewholders;
 
-import android.support.v4.content.ContextCompat;
 import android.support.v7.widget.RecyclerView;
 import android.util.TypedValue;
 import android.view.View;
