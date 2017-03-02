65ff4a331a483a6914b907290ff1b3e6b01f30c9
George G <kreach3r@users.noreply.github.com>
2017-03-02 21:12:11 +0100

    Android.mk: New KatsunaCommon setup
    * Added a way to merge AndroidManifests (like gradle should do)
    * Removed some obsolete comments
    * Enabled resources from KatsunaCommon
    * Removed emoji library

--- Android.mk
+++ Android.mk
@@ -1,4 +1,5 @@
 LOCAL_PATH := $(call my-dir)
+KATSUNA_COMMON_PATH := $(ANDROID_BUILD_TOP)/frameworks/KatsunaCommon
 include $(CLEAR_VARS)
 
 LOCAL_MODULE_TAGS := optional
@@ -6,6 +7,7 @@ LOCAL_MODULE_TAGS := optional
 LOCAL_SRC_FILES := $(call all-java-files-under, app/src/main)
 
 LOCAL_MANIFEST_FILE := app/src/main/AndroidManifest.xml
+LOCAL_FULL_LIBS_MANIFEST_FILES := $(KATSUNA_COMMON_PATH)/commons/src/main/AndroidManifest.xml
 LOCAL_RESOURCE_DIR := $(LOCAL_PATH)/app/src/main/res
 LOCAL_RESOURCE_DIR += frameworks/support/v7/appcompat/res
 LOCAL_RESOURCE_DIR += frameworks/support/v7/recyclerview/res
@@ -24,25 +26,18 @@ LOCAL_STATIC_JAVA_LIBRARIES += android-support-v4
 LOCAL_STATIC_JAVA_LIBRARIES += android-support-v7-appcompat
 LOCAL_STATIC_JAVA_LIBRARIES += android-support-v7-recyclerview
 LOCAL_STATIC_JAVA_LIBRARIES += android-support-design
-
-# Include the specified aar/jar (s), DEFINED in KatsunaCommon's Android.mk!
 LOCAL_STATIC_JAVA_LIBRARIES += picasso
 LOCAL_STATIC_JAVA_AAR_LIBRARIES := roundedimageview
-LOCAL_STATIC_JAVA_AAR_LIBRARIES += emojiconlibrary
 LOCAL_STATIC_JAVA_AAR_LIBRARIES += fabtransformation
 
 LOCAL_AAPT_INCLUDE_ALL_RESOURCES := true
 LOCAL_AAPT_FLAGS := --auto-add-overlay
 LOCAL_AAPT_FLAGS += --generate-dependencies
-# TODO: Don't know if actually needed
-LOCAL_AAPT_FLAGS += --extra-packages com.katsuna.common
+LOCAL_AAPT_FLAGS += --extra-packages com.katsuna.commons
 LOCAL_AAPT_FLAGS += --extra-packages android.support.v7.appcompat
 LOCAL_AAPT_FLAGS += --extra-packages android.support.v7.recyclerview
 LOCAL_AAPT_FLAGS += --extra-packages android.support.design
-# This is to include the aar's RESOURCES into this app
-# Notice the full packagename
 LOCAL_AAPT_FLAGS += --extra-packages com.makeramen.roundedimageview
-LOCAL_AAPT_FLAGS += --extra-packages com.rockerhieu.emojicon
 LOCAL_AAPT_FLAGS += --extra-packages com.konifar.fab_transformation
 
 LOCAL_PACKAGE_NAME := KatsunaMessages
@@ -52,14 +47,3 @@ LOCAL_CERTIFICATE := platform
 LOCAL_PROGUARD_ENABLED := disabled
 
 include $(BUILD_PACKAGE)
-
-include $(CLEAR_VARS)
-
-# Define here, which extra jar/aar this app needs
-# These should NOT be included in KatsunaCommon
-# These should reside inside app/libs of this app
-LOCAL_PREBUILT_STATIC_JAVA_LIBRARIES += emojiconlibrary:aosp/libs/emojiconlibrary-1.3.3.aar 
-
-include $(BUILD_MULTI_PREBUILT)
-
-include $(call all-makefiles-under,$(LOCAL_PATH))
